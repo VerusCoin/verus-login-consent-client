9be6522d860e0607166896e26071dd76bada6803
michaeltout <toutonghi.michael@gmail.com>
2021-12-17 19:58:36 +0100

    Add build script, disable devmode, remove option from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -170,7 +170,6 @@ module.exports = {
         uglifyOptions: {
           warnings: false,
           compress: {
-            ie8: false,
             conditionals: true,
             unused: true,
             comparisons: true,
